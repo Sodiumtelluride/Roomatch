f1e2eba68f56313441e222b66c0eea03062be77e
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-12-15 11:12:47 -0500

    Created card skeleton

---
+++ frontend/src/components/card/Card.jsx
@@ -0,0 +1,25 @@
+export default function Card(props){
+    return(
+        <div className="card">
+            <img src={props.img} alt="" className="card-img"/>
+            <div className="user-title">
+                <h1 className="user-name">{props.name}</h1> 
+                <h2 className="pronouns">{props.pronouns}</h2>
+            </div>
+            <div className="user-info">
+                <h3 className="info-heading">Description</h3>
+                <p className="des-txt">
+                    {props.description}
+                </p>
+                <h3 className="info-heading">Major</h3>
+                <p className="des-txt">
+                    {props.major}
+                </p>
+                <h3 className="info-heading">Class</h3>
+                <p className="des-txt">
+                    {props.class}
+                </p>
+            </div>
+        </div>
+    )
+}
