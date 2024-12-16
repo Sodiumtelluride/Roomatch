f2c2abb90561a44606894847291ee18a9f675627
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-12-16 11:12:31 -0500

    Message preview skeleton

--- frontend/src/components/messageDashboard/MessageDashboard.jsx
+++ frontend/src/components/messageDashboard/MessageDashboard.jsx
@@ -2,11 +2,15 @@ import './MessageDashboard.css';
 import Navbar from '../navbar/navbar';
 import Message from '../message/Message';
 import PFP from '../../assets/UserPhoto.png';
+import MessagePreview from '../messagePreview/MessagePreview';
 export default function MessageDashboard() {
     return(
-        <div className="MessageDashboard">
+        <>
             <Navbar/>
-            <Message user="Alice" message="Hi my name is nate i was wondering if you wanted to be my roomate? I promise im normal! i only eat birds sometimes" timeDelivered="2:34 AM" pfp={PFP}/>
-        </div>
+            <div className="MessageDashboard">
+                <Message user="Nate" message="Hi my name is nate i was wondering if you wanted to be my roomate? I promise im normal! i only eat birds sometimes" timeDelivered="2:34 AM" pfp={PFP}/>
+                <MessagePreview name="Nate" lastMessage="Hi my name is nate..." lastSent="10m" numUnread="1" pfp={PFP}/>
+            </div>
+        </>
     )
 }
---
+++ frontend/src/components/messagePreview/MessagePreview.jsx
@@ -0,0 +1,13 @@
+import './MessagePreview.css';
+
+export default function MessagePreview(props) {
+    return(
+        <div className="message-preview-container">
+            <img className='pfp-preview' src={props.pfp} />
+            <h1 className='name-preview'>{props.name}</h1>
+            {props.isTyping ? <p className='typing'>Typing...</p> : <p className='message-preview'>{props.lastMessage}</p>}
+            <p className='lastSent'>{props.lastSent}</p>
+            <p className='unread'>{props.numUnread}</p>
+        </div>
+    )
+}
