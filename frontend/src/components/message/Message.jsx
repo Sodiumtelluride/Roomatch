c3790d512b1bd5a886c49ebf42cb8cd04b957061
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-12-16 10:43:37 -0500

    Finished Message Design

--- frontend/src/components/message/Message.jsx
+++ frontend/src/components/message/Message.jsx
@@ -2,12 +2,15 @@ import './Message.css';
 
 export default function Message(props) {
     return(
-        <div className="Message">
+        <div className="message-container">
             <div className="message-content">
                 <h1>{props.user}</h1>
                 <p>{props.message}</p>
             </div>
-            
+            {/* <div className="message-info"> */}
+                <div className="time">{props.timeDelivered}</div>
+                <img className="pfp" src={props.pfp}></img>
+            {/* </div> */}
         </div>
     )
 }
--- frontend/src/components/messageDashboard/MessageDashboard.jsx
+++ frontend/src/components/messageDashboard/MessageDashboard.jsx
@@ -1,11 +1,12 @@
 import './MessageDashboard.css';
 import Navbar from '../navbar/navbar';
 import Message from '../message/Message';
+import PFP from '../../assets/UserPhoto.png';
 export default function MessageDashboard() {
     return(
         <div className="MessageDashboard">
             <Navbar/>
-            <Message user="Alice" message="Hello Bob!"/>
+            <Message user="Alice" message="Hi my name is nate i was wondering if you wanted to be my roomate? I promise im normal! i only eat birds sometimes" timeDelivered="2:34 AM" pfp={PFP}/>
         </div>
     )
 }
