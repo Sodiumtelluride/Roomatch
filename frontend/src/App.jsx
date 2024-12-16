da4b85ca4ded868bd8c0d29b99dd136bcb4d85a8
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-12-15 22:22:41 -0500

    General Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/navbar.jsx'
 import Card from './components/card/Card.jsx'
-import PFP from './assets/UserPhoto.png'
+import LowerTaperFade from './components/LowTaperFade/LowTaperFade.jsx'
+import CardGrid from './components/cardGrid/CardGrid.jsx'
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Card
-      img = {PFP}
-      name = "Nate Gelfand"
-      pronouns = "He/Him/His"
-      description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
-      major = "Computer Science"
-      class = "2025"
-      />
+      <CardGrid/>
+      <LowerTaperFade/>
     </>
   )
 }
