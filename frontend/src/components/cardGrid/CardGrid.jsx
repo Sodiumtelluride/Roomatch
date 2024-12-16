5282cea5fd4c51f7a76288d9f7fb0d4c3e647345
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-12-15 22:22:31 -0500

    Created Card Grid

---
+++ frontend/src/components/cardGrid/CardGrid.jsx
@@ -0,0 +1,74 @@
+import './CardGrid.css'
+import Card from '../card/Card.jsx'
+import PFP from '../../assets/UserPhoto.png'
+
+export default function CardGrid(){
+    return(
+        <div className="CardGrid">
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+            <Card
+            img = {PFP}
+            name = "Nate Gelfand"
+            pronouns = "He/Him/His"
+            description = "Hi im Nate! Im a freshman studying CS I love to play league for 10 hours a day. I enjoy Jorkin  it and eating watermelons"
+            major = "Computer Science"
+            class = "2025"
+            />
+        </div>
+    )
+}
