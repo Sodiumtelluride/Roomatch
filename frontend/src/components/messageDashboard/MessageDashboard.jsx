88d92ace0ef2f201a48c75dc4f043a6db936ba89
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-12-16 09:37:13 -0500

    Set Up Message dashboard

---
+++ frontend/src/components/messageDashboard/MessageDashboard.jsx
@@ -0,0 +1,9 @@
+import './MessageDashboard.css';
+import Navbar from '../navbar/navbar';
+export default function MessageDashboard() {
+    return(
+        <div className="MessageDashboard">
+            <Navbar/>
+        </div>
+    )
+}
