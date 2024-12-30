9c90a50523bbc8418a981b378c930ea392b160a0
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-12-30 17:53:52 -0500

    Basic UI

--- client/src/components/createUserForm/CreateUserForm.jsx
+++ client/src/components/createUserForm/CreateUserForm.jsx
@@ -2,16 +2,23 @@ import './CreateUserForm.css';
 
 function CreateUserForm() {
     return(
-        <form action="post">
-            <label className="form-lable" htmlFor="email">Email:</label><br/>
-            <input type="text" id="email" name="email"/><br/>
-            <label className="form-lable" htmlFor="password">Password:</label><br/>
-            <input type="text" id="password" name="password"/><br/>
-            <label htmlFor="first-name" className="form-lable">First Name</label>
-            <input type="text" id="first-name" name="first-name"/>
-            <label htmlFor="last-name" className="form-lable">Last Name</label>
-            <input type="text" id="last-name" name="last-name"/>
-            <input type="submit" value="Submit"/>
+        <form id='create-user-form' action="post">
+            <h1>Create An Account</h1>
+            <div className="name">
+                <div className="first-name">
+                    <label htmlFor="first-name" className="form-lable">First Name</label>
+                    <input type="text" id="first-name" name="first-name" required/>
+                </div>
+                <div className="last-name">
+                    <label htmlFor="last-name" className="form-lable">Last Name</label>
+                    <input type="text" id="last-name" name="last-name" required/>
+                </div>
+            </div>
+            <label className="form-lable" htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required/>
+            <label className="form-lable" htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required/>
+            <button type='submit'>Create Account</button>
         </form>
     )
 }
