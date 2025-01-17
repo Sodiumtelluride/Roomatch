b3bc28130e0c5c18264995dfd042339a74b365a0
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2025-01-16 22:54:04 -0500

    Image uploads to bucket succesfully

--- server/routes/updateMe.js
+++ server/routes/updateMe.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+
+const crypto = require('crypto'); 
+
 
 
 router.post('/updateMe', async (req, res) => {
     console.log( req.file);
+    const randomFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
     const dynamoDB = new AWS.DynamoDB.DocumentClient();
     const tableName = process.env.DYNAMODB_TABLE_NAME;
+    const bucketName = process.env.S3_BUCKET_NAME;
+    const region = process.env.AWS_REGION;
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
     const userId = req.user.user_id;
     const { first_name, last_name, email, password, display_name, pronouns, major, class_year, placeOrigin, description, extraversion, cleanliness, using_my_stuff, end_time, start_time } = req.body;
     const params = {
@@ -15,6 +24,23 @@ router.post('/updateMe', async (req, res) => {
             user_id: userId
         }
     };
+    const imageName = randomFileName();
+    const s3 = new S3Client({
+        credentials: { 
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey
+        },
+        region: region
+    });
+
+    const s3Params = {
+        Bucket: bucketName,
+        Key: imageName,
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype
+    };
+
+    const command = new PutObjectCommand(s3Params);
 
     try {
         console.log("running");
@@ -33,7 +59,7 @@ router.post('/updateMe', async (req, res) => {
             Key: {
             user_id: userId
             },
-            UpdateExpression: 'set first_name = :fn, last_name = :ln, email = :em, password = :pw, user_info.display_name = :dn, user_info.pronouns = :pr, user_info.major = :mj, user_info.#yr = :yr, user_info.placeOrigin = :po, user_info.description = :ds, user_info.extraversion = :ex, user_info.cleanliness = :cl, user_info.using_my_stuff = :us, user_info.end_time = :et, user_info.start_time = :st',
+            UpdateExpression: 'set first_name = :fn, last_name = :ln, email = :em, password = :pw, user_info.display_name = :dn, user_info.pronouns = :pr, user_info.major = :mj, user_info.#yr = :yr, user_info.placeOrigin = :po, user_info.description = :ds, user_info.extraversion = :ex, user_info.cleanliness = :cl, user_info.using_my_stuff = :us, user_info.end_time = :et, user_info.start_time = :st, images.image_1_name = :im',
             ExpressionAttributeValues: {
             ':fn': first_name || null,
             ':ln': last_name || null,
@@ -49,7 +75,8 @@ router.post('/updateMe', async (req, res) => {
             ':cl': cleanliness || null,
             ':us': using_my_stuff || null,
             ':et': end_time || null,
-            ':st': start_time || null
+            ':st': start_time || null,
+            ':im': imageName || null
             },
             ExpressionAttributeNames: {
             '#yr': 'class'
@@ -60,7 +87,7 @@ router.post('/updateMe', async (req, res) => {
 
         const updatedResult = await dynamoDB.update(updateParams).promise();
         console.log("updated item: " + JSON.stringify(updatedResult.Attributes));
-        // Remove the password field from the result
+        await s3.send(command);
         res.status(201).json("Updated user");
         } catch (error) {
         res.status(500).json({ error: error.message });
