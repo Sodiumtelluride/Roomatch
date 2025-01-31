9fdc8ad77a70b20e58ea7c4cac0416a18337d2d5
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2025-01-30 21:36:35 -0500

    Images in expanded work

--- client/src/components/cardExpanded/cardExpanded.jsx
+++ client/src/components/cardExpanded/cardExpanded.jsx
@@ -10,17 +10,27 @@ export default function CardExpanded(props) {
     
     const pronouns = props.pronouns === "Prefer Not to Specify" ? '' : props.pronouns;
 
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const handleBackClick = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? props.img.length - 1 : prevIndex - 1));
+    };
+
+    const handleForwardClick = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex === props.img.length - 1 ? 0 : prevIndex + 1));
+    };
+
     return (
         <div className="background">
             <div className="card-expanded">
                 <div className="card-banner-expanded">
                     <div className="img-container">
-                        <img src={props.img} alt="" className="card-img-expanded" />
-                </div>
-                
+                        <img src={props.img[currentImageIndex]} alt="" className="card-img-expanded" />
+                    </div>
+
                     <div className="button-container">
-                        <button className="back-button">Back</button>
-                        <button className="foward-button">Forward</button>
+                        <button className="back-button" onClick={handleBackClick}>Back</button>
+                        <button className="forward-button" onClick={handleForwardClick}>Forward</button>
                     </div>
                 </div>
 
@@ -43,24 +53,14 @@ export default function CardExpanded(props) {
                             <p className="des-txt-expanded">{props.description}</p>
                         </div>
 
-                        <div
-                        className="extraversion-expanded"
-                        style={{
-                            '--extraversion-fill': `${extraversionFillPercentage}%`, 
-                        }}
-                        >
+                        <div className="extraversion-expanded">
                             <h3 className="info-heading-expanded">Extraversion</h3>
                             <div className="fill-bar">
                                 <div className="fill" id="extraversion-fill"></div>
                             </div>
                         </div>
 
-                        <div
-                        className="cleanliness-expanded"
-                        style={{
-                            '--cleanliness-fill': `${cleanlinessFillPercentage}%`, 
-                        }}
-                        >
+                        <div className="cleanliness-expanded">
                             <h3 className="info-heading-expanded">Cleanliness</h3>
                             <div className="fill-bar">
                                 <div className="fill" id="cleanliness-fill"></div>
