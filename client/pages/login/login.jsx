04a1a499f210840d1d8fcf890a96d93dab01de1c
Sodiumtelluride <gelfandnat@gmail.com>
2025-02-02 18:26:12 -0500

    Added media queries for login

--- client/pages/login/login.jsx
+++ client/pages/login/login.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "../../src/index.css"
+import "./login.css"
 import LoginForm from '../../src/components/loginForm/LoginForm.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
