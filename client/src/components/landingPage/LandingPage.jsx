c5f3e59693332d8e33f619c0ae09c37a98f78fc9
Sodiumtelluride <gelfandnat@gmail.com>
2025-02-02 17:33:51 -0500

    Increased responsiveness of landing page

--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -9,7 +9,7 @@ import DownArrow from '../../assets/DownArrow.svg';
 import FeedImg from '../../assets/FeedImg.png';
 import MessagesImg from '../../assets/MessagesImg.png';
 import ProfileImg from '../../assets/ProfileImg.png';
-
+import NavbarLandingPage from '../navbarLandingPage/NavbarLandingPage.jsx';
 function LandingPage() {
 
     const appear = (element) => {
@@ -35,27 +35,26 @@ function LandingPage() {
         const sampleMessages = document.querySelector('.message.container');
         const sampleProfile = document.querySelector('.profile.container');
         const downArrow = document.querySelector('.down-arrow-container');
-        const feedPoint = document.querySelector('.feed.point').getBoundingClientRect().top - 200;
-        const messagePoint = document.querySelector('.message.point').getBoundingClientRect().top + 400;
-        const profilePoint = document.querySelector('.profile.point').getBoundingClientRect().top + 1000;
         if (sampleFeed) disappearInstant(sampleFeed);
         if (sampleMessages) disappearInstant(sampleMessages);
         if (sampleProfile) disappearInstant(sampleProfile);
         const handleScroll = () => {
+            const windowHeight = window.innerHeight;
+
             if (window.scrollY <= 100) {
-                appear(downArrow);
+            appear(downArrow);
+            } else {
+            disappear(downArrow);
             }
-            else {
-                disappear(downArrow);
-            }
-            if (window.scrollY >= feedPoint) {
-                appear(sampleFeed);
+
+            if (sampleFeed.getBoundingClientRect().top <= windowHeight) {
+            appear(sampleFeed);
             } 
-            if (window.scrollY >= messagePoint) {
-                appear(sampleMessages);
+            if (sampleMessages.getBoundingClientRect().top <= windowHeight) {
+            appear(sampleMessages);
             } 
-            if (window.scrollY >= profilePoint) {
-                appear(sampleProfile);
+            if (sampleProfile.getBoundingClientRect().top + 200 <= windowHeight) {
+            appear(sampleProfile);
             }
         };
 
@@ -66,31 +65,34 @@ function LandingPage() {
     }, []);
 
     return (
-        <div className="landing-page">
-            <h1 className="main-tagline gradient">Start Your Semester the Right Way.</h1>
-            <div className="sample-message-container"> 
-                <img className="sample-profile" src={SampleProfile}/>
-                <h1 className="sample-message-text">Hi! Are you looking for a roommate?</h1>
+        <>
+            <NavbarLandingPage/>
+            <div className="landing-page">
+                <h1 className="main tagline gradient">Start Your Semester the Right Way.</h1>
+                <div className="sample-message-container"> 
+                    <img className="sample-profile" src={SampleProfile}/>
+                    <h1 className="sample-message-text">Hi! Are you looking for a roommate?</h1>
+                </div>
+                <div className="down-arrow-container">
+                    <img className="down-arrow" src={DownArrow} alt="Down Arrow" />
+                </div>
+                <div className="feed point"></div>
+                <div className="feed container">
+                    <h1 className="feed tagline gradient">Find A Perfect Match With a Detailed Roommate Feed</h1>
+                    <img className="feed image" src={FeedImg} alt="Feed" />
+                </div>
+                <div className="message point"></div>
+                <div className="message container">
+                    <h1 className="message tagline gradient">Message and Connect With Potential Rooommates </h1>
+                    <img className="message image" src={MessagesImg} alt="Message" />
+                </div>
+                <div className="profile point"></div>
+                <div className="profile container">
+                    <h1 className="profile tagline gradient">Customize Your Profile to Put Yourself Out There </h1>
+                    <img className="profile image" src={ProfileImg} alt="Profile" />
+                </div>
             </div>
-            <div className="down-arrow-container">
-                <img className="down-arrow" src={DownArrow} alt="Down Arrow" />
-            </div>
-            <div className="feed point"></div>
-            <div className="feed container">
-                <h1 className="feed-tagline gradient">Find A Perfect Match With a Detailed Roommate Feed</h1>
-                <img className="feed image" src={FeedImg} alt="Feed" />
-            </div>
-            <div className="message point"></div>
-            <div className="message container">
-                <h1 className="message-tagline gradient">Message and Connect With Potential Rooommates </h1>
-                <img className="message image" src={MessagesImg} alt="Message" />
-            </div>
-            <div className="profile point"></div>
-            <div className="profile container">
-                <h1 className="profile-tagline gradient">Customize Your Profile to Put Yourself Out There </h1>
-                <img className="profile image" src={ProfileImg} alt="Profile" />
-            </div>
-        </div>
+        </>
     )
 }
 export default LandingPage;
