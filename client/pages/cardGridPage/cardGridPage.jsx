74b6b5201770e0684fd70733765c29f93525fee4
Sodiumtelluride <gelfandnat@gmail.com>
2025-02-03 14:10:40 -0500

    Added media queries for card grid

--- client/pages/cardGridPage/cardGridPage.jsx
+++ client/pages/cardGridPage/cardGridPage.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./cardGridPage.css"
+// import '../../src/components/card/Card.css'
 import CardGrid from "../../src/components/cardGrid/CardGrid.jsx";
+import "./cardGridPage.css"
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <CardGrid />
-  </StrictMode>,
+    <StrictMode>
+        <CardGrid />
+    </StrictMode>,
 )
