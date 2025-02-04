004384a91d9993b3f924953f7c245a23b6dc8fd2
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2025-02-04 14:12:16 -0500

    Email redirection works

--- server/routes/verifyEmail.js
+++ server/routes/verifyEmail.js
@@ -27,12 +27,8 @@ router.get('/verify-email', async (req, res) => {
             },
             ReturnValues: 'ALL_NEW'
         };
-
         const result = await dynamoDB.update(params).promise();
-        console.log('User verified:', result);
-
-        // Redirect to a confirmation page or send a success response
-        res.status(200).json({ message: 'Email verified successfully' });
+        res.redirect('http://localhost:5173/pages/login/login.html');
     } catch (error) {
         console.error('Error verifying email:', error);
         res.status(400).json({ error: 'Invalid or expired token' });
