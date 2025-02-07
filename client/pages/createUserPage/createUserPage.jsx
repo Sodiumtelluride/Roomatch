a48fefef054cca76852fb234b1cbbf6a9fb1751c
Sodiumtelluride <gelfandnat@gmail.com>
2025-02-06 22:26:23 -0500

    Added media queries to user profile

--- client/pages/createUserPage/createUserPage.jsx
+++ client/pages/createUserPage/createUserPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../../src/index.css";
+
 import CreateUserForm from "../../src/components/createUserForm/CreateUserForm.jsx";
 
 createRoot(document.getElementById("root")).render(
--- client/pages/userPage/userPage.jsx
+++ client/pages/userPage/userPage.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import "../../src/index.css"
 import UserProfile from '../../src/components/userProfile/userProfile.jsx'
 import Navbar from '../../src/components/navbar/navbar.jsx'
+import "./userPage.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
